9f7e5b4e91db8922722607a0f3802b062f1f2ba6
Wang Kuan <wangkuan42@gmail.com>
2023-07-11 23:24:14 +0200

    commit

--- 4067827_Wang_Kuan/src/de/fernuni/kurs01584/ss23/algorithmus/DschungelGenerator.java
+++ 4067827_Wang_Kuan/src/de/fernuni/kurs01584/ss23/algorithmus/DschungelGenerator.java
@@ -1,7 +1,168 @@
 package de.fernuni.kurs01584.ss23.algorithmus;
 
-//import de.fernuni.kurs01584.ss23.modell.*;
+import java.util.*;
+
+import de.fernuni.kurs01584.ss23.dateiverarbeitung.DateneingabeXML;
+import de.fernuni.kurs01584.ss23.modell.*;
 
 public class DschungelGenerator {
+    public boolean erzeugProbleminstanzen(String xmlEingabedatei, String xmlAusgabedatei) throws Exception {
+        // laden der vorgegebenen Probleminstanz
+        Schlangenjagd problemInstanz = DateneingabeXML.parseXML(xmlEingabedatei);
+        // Prüfen die Vollständigkeit der Daten
+        if (!checkProbleminszanz(problemInstanz)) {
+            System.out.println("Pflichtdaten fehlen in vorgegebene Probleminstanz.");
+            return false;
+        }
 
+        // Vervollständigung der Probleminstanz mit Hilfe des Dschungelgenerators
+
+        // Prüfen die Vorgabe der Zeitlimit und Default Wert einstellen
+        if (problemInstanz.getZeit().getVorgabe() == 0.0 || problemInstanz.getZeit().getEinheit().equals(null)) {
+            problemInstanz.getZeit().setEinheit("ms");
+            problemInstanz.getZeit().setVorgabe(2000.0);
+        }
+
+        // Erzeugung eines Dschungels entsprechend der gegebenen Vorgaben
+        if (problemInstanz.getDschungel().getFelder().equals(null) || problemInstanz.getDschungel().getFelder().isEmpty()) {}
+        else {
+            // löschen bereits vorhandene Dschungelfelder in problemInstanz und neue Felder erstellen
+            List <Feld> newFelder = new ArrayList<>();
+            for (int FeldId = 0; FeldId < problemInstanz.getDschungel().getSpalten() * problemInstanz.getDschungel().getZeilen(); FeldId++) {
+                Feld feld = new Feld();
+                feld.setId("F" + FeldId);
+                feld.setSpalte(FeldId % problemInstanz.getDschungel().getSpalten());
+                feld.setZeile(FeldId / problemInstanz.getDschungel().getZeilen());
+                feld.setVerwendbarkeit(1);
+                feld.setPunkte(1);
+                newFelder.add(feld);
+            }
+            problemInstanz.getDschungel().setFelder(newFelder);
+
+            List <Schlangenart> schlangenartList = new ArrayList<>();
+            for (Schlangenart schlangenart: schlangenartList) {
+                if (schlangenart.getVerwendbarkeit() > 0) {
+                    String zeichenkette = schlangenart.getZeichenkette();
+                    List<Schlangenglied> schlangengliedList = new ArrayList<>();
+
+                    for (int i = 0; i < zeichenkette.length(); i++) {
+                        char zeichen = zeichenkette.charAt(i);
+                        Schlangenglied schlangenGlied = new Schlangenglied();
+                        // Erzeugen des zulaessigeFelders
+                        List<Feld> zulaessigesFelder = erzeugZulaessigeFelder(newFelder);
+                        if (i == 0) {
+                            // StartFeld auswählen    
+                            setZufaelligFeldmitZeichen(zeichen,zulaessigesFelder,schlangenGlied);
+                            schlangengliedList.add(schlangenGlied);
+                        }
+                        else {
+                            setZufaelligFeldmitZeichen(zeichen,zulaessigesFelder,schlangenGlied);
+                            Schlangenglied vorherigesGlied = schlangengliedList.get(i - 1);
+                            List<Feld> nachbarFeld = erzeugNachbarFeld(newFelder, vorherigesGlied, schlangenGlied, schlangenart);
+                            if (nachbarFeld.isEmpty()) {
+                                // keine zulässige Nachbarfelder gefunden
+                                if (schlangengliedList.size() == zeichenkette.length()) {
+                                    // vollständige Schlange nach Schlangenart gefunden
+                                    schlangenart.setAnzahl(schlangenart.getVerwendbarkeit() - 1);
+                                }
+                                else {
+                                    // keine vollständige Schlange gefunden, Schlangenglieder müssen zurückgenommen werden
+                                    for (Schlangenglied schlangenglied : schlangengliedList) {
+                                        schlangenglied.getFeld().setZeichen(null);
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+
+            }
+        }
+
+        return true;
+    }
+
+    private boolean checkProbleminszanz(Schlangenjagd problemInstanz) {
+        if (problemInstanz.getDschungel().getZeilen() == 0 || problemInstanz.getDschungel().getSpalten() == 0) {
+            // Zeilen oder Spalten Anzahlen sind nicht definiert oder initialisiert
+            System.out.println("Anzahl der Zeilen und Spalten des Dschungels sind nicht vollständig in Eingabedatei angegeben.");
+            return false;
+        }
+        if (problemInstanz.getDschungel().getzeichenmenge().equals(null)) {
+            System.out.println("keine zulässige Zeichen wird für den Dschungel angegeben.");
+            return false;
+        }
+        if (problemInstanz.getSchlangenarten().equals(null)) {
+            System.out.println("keine zulässige Schlangenarten werden für den Dschungel angegeben.");
+            return false;
+        }
+        else {
+            List <Schlangenart> schlangenarten = new ArrayList<>();
+            for (Schlangenart schlangenart : schlangenarten) {
+                // Anzahl von Schlangenart sind nicht definiert oder initialisiert
+                if (schlangenart.getVerwendbarkeit() == 0) {
+                    System.out.println("keine erwünschte Anzahl von Schlangen wird für Schlangenart " + schlangenart.getId() + " angegeben.");
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    private List<Feld> erzeugZulaessigeFelder(List<Feld> felder) {
+        List<Feld> freiFeld = new ArrayList<>();
+        for (Feld feld: felder) {
+            if (feld.getZeichen().equals(null)) {
+                freiFeld.add(feld);
+            }
+        }
+
+        return freiFeld;
+    }
+
+    private void setZufaelligFeldmitZeichen(char zeichen, List<Feld>felder, Schlangenglied schlangenGlied) {
+        String zeichenStr = String.valueOf(zeichen);
+        // Erstellung eine randomgenerator
+        Random random = new Random();
+        int randomIndex = random.nextInt(felder.size());
+        Feld feld = felder.get(randomIndex);
+        feld.setZeichen(zeichenStr);
+        schlangenGlied.setFeld(feld);
+    }
+
+    private List<Feld> erzeugNachbarFeld(List<Feld> felder, Schlangenglied vorherigesGlied, Schlangenglied schlangenGlied, Schlangenart schlangenart){
+        List<Feld> nachbarFeld = new ArrayList<>();
+        Feld vorherigesGliedFeld = vorherigesGlied.getFeld();
+        Feld schlangenGliedFeld = schlangenGlied.getFeld();
+        for (Feld feld: felder) {
+            if (checkNachbarfeld(vorherigesGliedFeld, schlangenGliedFeld, schlangenart)) {
+                nachbarFeld.add(feld);
+            }
+        }
+
+        return nachbarFeld;
+    }
+
+    private boolean checkNachbarfeld(Feld vorherigesFeld, Feld aktuellesFeld, Schlangenart schlangenart){
+        String nachbarTyp = schlangenart.getNachStr().getTyp();
+        List<Parameter> parameters = schlangenart.getNachStr().getParameters();
+        if (nachbarTyp.equals("Distanz")) {
+            int parameter = parameters.get(0).getWert();
+            if (Math.abs(vorherigesFeld.getSpalte() - aktuellesFeld.getSpalte()) <= parameter && Math.abs(vorherigesFeld.getZeile() - aktuellesFeld.getZeile()) <= Math.abs(vorherigesFeld.getSpalte() - aktuellesFeld.getSpalte())) {
+                return true;
+            }
+        }
+        else {
+            // Es gibt zwei Parametern für Sprung Typ
+            int parameter_1 = parameters.get(0).getWert();
+            int parameter_2 = parameters.get(1).getWert();
+
+            if ((Math.abs(vorherigesFeld.getSpalte() - aktuellesFeld.getSpalte()) == parameter_1 && Math.abs(vorherigesFeld.getZeile() - aktuellesFeld.getZeile()) == parameter_2) || (Math.abs(vorherigesFeld.getSpalte() - aktuellesFeld.getSpalte()) == parameter_2 && Math.abs(vorherigesFeld.getZeile() - aktuellesFeld.getZeile()) == parameter_1)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
 }
